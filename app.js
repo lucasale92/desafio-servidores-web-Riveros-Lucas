fec988fe08c99e16f722ee581857568a7b198267
Lucas <riveros.lucas07@gmail.com>
2023-05-02 17:36:58 -0300

    commit

--- app.js
+++ app.js
@@ -1,32 +1,53 @@
-const express = require('express')
-const ProductManager = require('./productManager.js');
-const container = new ProductManager('./productos.json');
+// Importación del módulo express y la clase ProductManager
+import express from "express";
+import ProductManager from "./productManager.js";
 
+// Instanciación de la clase ProductManager y creación de la aplicación express
+const productManager = new ProductManager('./productos.json');
 const app = express();
-const PORT = 8080
+const PORT = 8080;
 
+// Middleware para parsear el cuerpo de las solicitudes HTTP
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.listen( PORT,() =>{
+// Inicialización del servidor HTTP
+app.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
-})
+});
 
-app.get('/products', async (req, res) =>{
-    const limit = req.query.limit;
-    const products = container.getProducts();
-    if (limit){
-        res.json (products.slice(0, limit));
-    } else {
-        res.json(products);
+// Ruta para obtener todos los productos
+app.get('/products', async (req, res) => {
+    try {
+        const limit = req.query.limit; // Obtiene el parámetro limit de la consulta (si existe)
+        const products = await productManager.getProducts(); // Obtiene todos los productos
+        if (limit) {
+            // Si se proporcionó un parámetro limit, devuelve sólo los primeros productos hasta el límite especificado
+            res.status(200).json(products.slice(0, limit));
+        } else {
+            // Si no se proporcionó un parámetro limit, devuelve todos los productos
+            res.status(200).json(products);
+        }
+    } catch (error) {
+        // Si se produce un error, devuelve un mensaje de error con un código de estado HTTP 500
+        res.status(500).json({message: "error"});
     }
-})
+});
 
-app.get('/products/id', async (req, res) =>{
-    const id = container.getProductsById(parseInt(id));
-    if (product) {
-        res.json(products);
-    } else{
-        res.json({error:"product not found"});
+// Ruta para obtener un producto por ID
+app.get('/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params; // Obtiene el parámetro ID de la URL
+        const products = await productManager.getProductsById(parseInt(id)); // Obtiene el producto por ID
+        if (!products) {
+            // Si no se encuentra ningún producto, devuelve un mensaje de error con un código de estado HTTP 400
+            return res.status(400).json({error: "product not found"});
+        } else {
+            // Si se encuentra el producto, devuelve los detalles del producto
+            res.status(200).json(products);
+        }
+    } catch (error) {
+        // Si se produce un error, devuelve un mensaje de error con un código de estado HTTP 500
+        res.status(500).json({message: "error"});
     }
-})
+});
--- productManager.js
+++ productManager.js
@@ -1,84 +1,115 @@
-const fs = require('fs');
+// Se importa el módulo "fs" para manejar archivos
+import fs from 'fs';
+
 class ProductManager {
-    constructor() {
-      this.path = 'productos.json';
-    }
-    async getProducts() {
-      try{
-        if(fs.existsSync(this.path)){
-          const data = await fs.promises.readFile(this.path, 'utf8');
-          return JSON.parse(data);
-        }
-        await fs.promises.writeFile(this.path, JSON.stringify([]));
-        return [];
-      } catch{error}{
-        throw new Error (error.message);
-      }
-    }
-    async addProduct(product) {
-      try{
-        let data = await this.getProducts();
-        const checkCode = data.find((p) => p.code === product.code);
-        if (checkCode) {
-          throw new Error('El codigo ya existe');
-        }
-        if (
-          !product.title ||
-          !product.description ||
-          !product.price ||
-          !product.thumbnail ||
-          !product.code ||
-          !product.stock
-        ) {
-          throw new Error('faltan campos por completar');
-        }
-        const lastProductId =
-        data.length > 0 ? data[data.length - 1].id+1:1;
-        console.log(lastProductId);
-        const newProduct = { ...product, id: lastProductId,}
-        data.push(newProduct);
-        const productString = JSON.stringify(this.products, null, 2)
-        await fs.promises.writeFile("productos.json", productString)
-        return newProduct
-      }
-      catch{error}{
-        throw new Error (error.message);
-      }
-    }   
-    // llamo al metodo getProductById
-    async getProductById(id) {
-      try{
-        let data = await this.getProducts();
-        const productFound = data.find((product) => product.id === id);
-        if (!productFound){
-          throw new Error("Product not found");
-        }
-          return productFound;
-      }
-      catch{error}{
-        throw new Error (error.message);
-      }  
-    }
-
-
-    // llamo al método updateProduct que actualiza los productos en el archivo JSON
-    async updateProduct(id, newData) {
-      let data = await this.getProducts();
-      const position = data.findIdex((product) => product.id === id)
-      if (position !== -1){
-        data[position] = {... data[position]}, newData;
-        const productString = JSON.stringify(data, null, 2)
-      await  fs.promises.writeFileSync('productos.json', productString)
-      return "product updated successfully"
-      }
-    }
-
-    deleteProduct(id) {
-      this.products = this.products.filter((product) => product.id !== id);
-      fs.writeFileSync("productos.json", JSON.stringify(this.products, null, 2));
-    }
+constructor() {
+// Ruta donde se encuentra el archivo "productos.json"
+this.path = './scr/productos.json';
 }
-  // Crea dos objetos de prueba
+
+// Método para obtener todos los productos
+async getProducts() {
+try {
+// Se verifica si existe el archivo "productos.json"
+if (fs.existsSync(this.path)) {
+// Se lee el archivo
+const data = await fs.promises.readFile(this.path, 'utf8');
+// Se convierte el archivo a un objeto JSON
+return JSON.parse(data);
+}
+// Si no existe el archivo se crea vacío
+await fs.promises.writeFile(this.path, JSON.stringify([]));
+// Se retorna un array vacío
+return [];
+} catch (error) {
+// Si ocurre algún error se captura y se lanza otro error personalizado
+throw new Error(error.message);
+}
+}
+
+// Método para agregar un producto
+async addProduct(product) {
+try {
+// Se obtienen todos los productos
+let data = await this.getProducts();
+// Se verifica si ya existe un producto con el mismo código
+const checkCode = data.find((p) => p.code === product.code);
+if (checkCode) {
+// Si existe, se lanza un error personalizado
+throw new Error('El código ya existe');
+}
+if (
+!product.title ||
+!product.description ||
+!product.price ||
+!product.thumbnail ||
+!product.code ||
+!product.stock
+) {
+// Si faltan campos por completar, se lanza un error personalizado
+throw new Error('Faltan campos por completar');
+}
+// Se obtiene el último ID de producto y se suma uno para el nuevo producto
+const lastProductId = data.length > 0 ? data[data.length - 1].id + 1 : 1;
+console.log(lastProductId);
+// Se crea un nuevo producto con el ID correspondiente
+const newProduct = { ...product, id: lastProductId };
+// Se agrega el nuevo producto al array de productos
+data.push(newProduct);
+// Se convierte el array de productos a una cadena JSON
+const productString = JSON.stringify(this.products, null, 2);
+// Se escribe la cadena JSON en el archivo "productos.json"
+await fs.promises.writeFile('productos.json', productString);
+return newProduct
+}
+catch{error}{
+throw new Error (error.message);
+}
+}
+
+
+// Se define una función async llamada getProductById que recibe como parámetro un ID
+async getProductById(id) {
+  try{
+  // Se obtiene el array de productos
+    let data = await this.getProducts(); 
+    // Se busca el producto correspondiente al ID
+  const productFound = data.find((product) => product.id === id); 
+  // Si no se encuentra el producto, se retorna null
+  if (!productFound){ 
+  return null
+  }
+  // Si se encuentra el producto, se retorna el objeto correspondiente
+  return productFound; 
+  }
+  // Si ocurre algún error se captura y se lanza otro error personalizado
+  catch{error}{ 
+  throw new Error (error.message);
+  }
+  }
+  
+  // Se define una función async llamada updateProduct que recibe como parámetro un ID y un objeto con la información actualizada
+  async updateProduct(id, newData) {
+    // Se obtiene el array de productos
+  let data = await this.getProducts(); 
+  // Se busca la posición en el array del producto correspondiente al ID
+  const position = data.findIdex((product) => product.id === id) 
+  // Si se encuentra la posición del producto, se actualiza su información
+  if (position !== -1){ 
+  data[position] = {... data[position]}, newData;
+  // Se convierte el array de productos a una cadena JSON
+  const productString = JSON.stringify(data, null, 2) 
+  // Se escribe la cadena JSON en el archivo "productos.json"
+  await fs.promises.writeFileSync('productos.json', productString) 
+   // Se retorna un mensaje de éxito
+  return "product updated successfully"
+  }
+  }
+
+deleteProduct(id) {
+// Se filtra el array de productos para eliminar el producto con el ID indicado
+this.products = this.products.filter((product) => product.id !== id);
+//
   const product1 = {
     title:"producto prueba",
     description: 'Este es un producto prueba',
@@ -95,6 +126,7 @@ class ProductManager {
   //   console.log(await productManager.addProduct(product1))
   //   console.log(await productManager.updateProduct(1, product2))
   //   console.log(await productManager.getProductById(400))
-}
+}}}
   //  asyncFn();
+
 module.exports = ProductManager;
